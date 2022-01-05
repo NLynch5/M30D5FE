493ebaa0ec8ee18e8f70d2b1bf400ea3f090ebc1
Luis RdV <job.wito.uk@gmail.com>
2022-01-05 11:26:51 +0000

    components created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with thisssss
         </a>
       </header>
     </div>
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+//this is a test 2
