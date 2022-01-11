09e682261b64720e39621792e240766a1239b6b9
Natalie Lynch <natalielynch@Natalies-MacBook-Air.local>
2022-01-11 17:02:48 +0000

    updated calc code added to main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import React from "react";
 
+//Natalie added useState
+//import React, { useState } from "react";
+
 import Profile from "./components/profile";
 import Login from "./components/login";
 import Signup from "./components/signup";
+// natalie added import below to calc
+import CalcForm from "./components/calculatorForm.js";
+import CreditTotal from "./components/creditTotal";
 
 import "./App.css";
 import "./components/profile.css";
-
-
+import "./components/login.css";
+//natalie added below
+import "./components/calculatorForm.css";
+import "./components/calculatorForm.js";
 
 const App = () => {
   return (
@@ -24,7 +32,7 @@ const App = () => {
                 <Link id="link" to="/profile"></Link>
               </li>
               <li className="endpoint">
-                <Link to="/calculator"> </Link>
+                <Link to="/calculator">CALC </Link>
               </li>
             </ul>
           </nav>
@@ -48,8 +56,30 @@ const App = () => {
   );
 };
 
+//Natalie changed below code
 const Calculator = () => {
-  return <h2>Calculator</h2>;
+  return (
+    <div className="calcContainer">
+      <nav className="calcNavBar">
+        Logo <button className="profileBtn"> My Profile </button>
+        <button className="logoutBtn"> Log Out</button>
+      </nav>
+      <header className="calcHeader">
+        <h2>Carbon Footprint Calculator</h2>
+      </header>
+      <div className="calcForm">
+        <CalcForm />
+      </div>
+      <label>
+        Total Credits
+        <button className="calcTotal">{/* <CreditTotal /> */}</button>
+      </label>
+      <footer className="calcFooter">
+        Please go to the 'Your Profile' page to see how you can use your credits
+        at your next gaming event.
+      </footer>
+    </div>
+  );
 };
 
 // const Profile = () => {
